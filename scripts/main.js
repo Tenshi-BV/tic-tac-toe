95e63db8c74a78695d6ed8253311b2e2016d35ca
Blaz Vrataric <online.blaz@gmail.com>
2021-04-13 10:21:49 +0200

    minor html and css adjustments, redid logic, added field array and click events for signs

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,75 @@
 const gameBoard = (() => {
-    const updateScore = (x, y) => {
-        const score = document.querySelector('#score');
-        const player1DOM = document.querySelector('#player1');
-        player1DOM.textContent = x;
-        score.appendChild(player1DOM);
-        const player2DOM = document.querySelector('#player2');
-        player2DOM.textContent = y;
-        score.appendChild(player2DOM);
-    }
 
-    const playerFactory = (name, computer) => {
-        return { name, computer };
-    };11
-    const gameStart = (() => {
-        const start = document.querySelector('#score');
-        const startButton = start.createElement('div');
-        startButton.setAttribute('id', 'start');
-        start.appendChild(startButton);
+    const field1 = document.querySelector('#one>div');
+    const field2 = document.querySelector('#two>div');
+    const field3 = document.querySelector('#three>div');
+    const field4 = document.querySelector('#four>div');
+    const field5 = document.querySelector('#five>div');
+    const field6 = document.querySelector('#six>div');
+    const field7 = document.querySelector('#seven>div');
+    const field8 = document.querySelector('#eight>div');
+    const field9 = document.querySelector('#nine>div');
 
-        return { player1, player2 }
+    const playerFactory = (name) => {
+        return { name };
+    };
+
+    let displayController = [
+        { id: "one", field: field1, sign: 0 },
+        { id: "two", field: field2, sign: 0 },
+        { id: "three", field: field3, sign: 0 },
+        { id: "four", field: field4, sign: 0 },
+        { id: "five", field: field5, sign: 0 },
+        { id: "six", field: field6, sign: 0 },
+        { id: "seven", field: field7, sign: 0 },
+        { id: "eight", field: field8, sign: 0 },
+        { id: "nine", field: field9, sign: 0 },
+    ];
+
+    const gameFlow = (() => {
+        const board = document.querySelectorAll('#board>div');
+        board.forEach((div) => {
+            div.addEventListener('click', () => {
+                deploy(div.id);
+            })
+        });
+
+        const deploy = (x) => {
+            let toSwitch = displayController.find(box => box.id == x);
+            if (toSwitch.sign == 0) {
+                if (turn == "player1") {
+                    toSwitch.sign = "circle";
+                } else {
+                    toSwitch.sign = "cross";
+                }
+                showBoard();
+                checkBoard();
+                switchPlayer();
+            }
+        };
+
+        const showBoard = () => {
+            displayController.filter(box => {
+                if (box.sign == "circle") {
+                    box.field.classList.add('circle')
+                } else if (box.sign == "cross") {
+                    box.field.classList.add('cross')
+                } else { }
+            })
+        };
+
+        let turn = "player1";
+        const switchPlayer = () => {
+            if (turn == "player1") {
+                turn = "player2"
+            } else {
+                turn = "player1"
+            }
+        }
+
+        const checkBoard = () => {
+            switch(){}
+        }
     })();
-    let displayController = [];
-    const gameFlow = (() => {})();
-})();
+
+})()
